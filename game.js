9255dc74320eef71d17700e89972a2fac8beaf12
Bhuynee <dangthivanvan3@gmail.com>
2025-12-10 23:14:19 +0700

    Delete game.js

--- game.js
+++
@@ -1,8 +0,0 @@
-let autoOpen=false;
-const road=document.getElementById('road');
-function addRoad(r){const d=document.createElement('div');d.className='dot '+r;road.appendChild(d);}
-function playRound(){
- rollDice();
- setTimeout(()=>{if(autoOpen)toggleCup();
- addRoad(Math.random()>0.5?'tai':'xiu');},1200);
-}
